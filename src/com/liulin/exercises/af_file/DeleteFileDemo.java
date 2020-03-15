aa9cf51133a85ecc8450c0f281750eb7911714ba
zgliulin <larry_mall@163.com>
2020-03-15 19:06:36 +0800

    20200315 基础语法-删除文件夹及文件

---
+++ src/com/liulin/exercises/af_file/DeleteFileDemo.java
@@ -0,0 +1,29 @@
+package com.liulin.exercises.af_file;
+
+import java.io.File;
+
+/**
+ * Create by DbL on 2020/3/15 0015
+ */
+public class DeleteFileDemo {
+    public static void main(String args[]) {
+        // 这里修改为自己的测试目录
+        File folder = new File("/tmp/java/");
+        deleteFolder(folder);
+    }
+
+    // 删除文件及目录
+    public static void deleteFolder(File folder) {
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File f : files) {
+                if (f.isDirectory()) {
+                    deleteFolder(f);
+                } else {
+                    f.delete();
+                }
+            }
+        }
+        folder.delete();
+    }
+}
